2d5b3a8455401c83c6886d5978594a990f49c9e1
Detlef Gerhardt <feedback@dgerhardt.de>
2024-10-15 14:41:05 +0200

    Frst commit

--- Visual Studio/ItelexCommon/Private/PrivateConstants.cs
+++ Visual Studio/ItelexCommon/Private/PrivateConstants.cs
@@ -15,8 +15,10 @@ namespace ItelexCommon
 		public const string TELEGRAM_BOT_TOKEN = ""; // itelex_bot
 		public const string TELEGRAM_TESTBOT_TOKEN = ""; // itelex_test_bot
 
-		public const int ITELEX_905259_PIN = 0;
+		public const int ITELEX_DEBUG_NUMBER = 905259;
+		public const int ITELEX_DEBUG_PIN = 2535;
 
+		public const string DEBUG_EMAIL_ADDRESS = "mail@dgerhardt.de";
 		public const string MSGSRV_FAXNUMBER = "+49 ...";
 
 		public const string TELEX_TELEXGATE_PASSWORD = "password";
